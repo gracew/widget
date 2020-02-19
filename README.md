ce7b9b6a93b12a0e7c0f4dee68184ebcf5aac592
Grace Wang <gracewang92@gmail.com>
2020-02-19 13:24:11 -0800

    add API schema, regenerate

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # widget
 
+Run the server
 ```
 go run server.go
+```
+
+Regenerate after changes to `schema.graphqls`
+```
+go run github.com/99designs/gqlgen
 ```
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -10,14 +10,12 @@ import (
 	"github.com/gracew/widget/graph/model"
 )
 
-func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+func (r *mutationResolver) CreateAPI(ctx context.Context, input model.NewAPI) (*model.API, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	u := model.User{ID: "1", Name: "Grace"}
-	todo := model.Todo{ID: "1", Text: "Buy groceries", Done: true, User: &u}
-	return []*model.Todo{&todo}, nil
+func (r *queryResolver) Apis(ctx context.Context) ([]*model.API, error) {
+	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
@@ -25,3 +23,18 @@ func (r *Resolver) Query() generated.QueryResolver       { return &queryResolver
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	u := model.User{ID: "1", Name: "Grace"}
+	todo := model.Todo{ID: "1", Text: "Buy groceries", Done: true, User: &u}
+	return []*model.Todo{&todo}, nil
+}
