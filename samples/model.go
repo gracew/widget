49af355d0f0a8bf94861efca42fa12d4952e1f94
Grace Wang <gracewang92@gmail.com>
2020-03-15 01:59:13 -0700

    delete sample file

--- samples/model.go
+++
@@ -1,7 +0,0 @@
-package generated
-
-type Object struct {
-	ID        string `json:"id" sql:"type:uuid,default:gen_random_uuid()"`
-	CreatedBy string `json:"createdBy"`
-	Foo	  string `json:"bar"`
-}
