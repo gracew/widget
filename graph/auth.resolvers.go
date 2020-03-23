08aed7373825e93d93cce2290dc4b3070b0a97ca
Grace Wang <gracewang92@gmail.com>
2020-03-22 17:02:14 -0700

    update manual model

--- graph/auth.resolvers.go
+++ graph/auth.resolvers.go
@@ -10,10 +10,6 @@ import (
 	"github.com/gracew/widget/graph/model"
 )
 
-func (r *authResolver) DeletePolicy(ctx context.Context, obj *model.Auth) (*model.AuthPolicy, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
 func (r *mutationResolver) AuthAPI(ctx context.Context, input model.AuthAPIInput) (bool, error) {
 	err := r.Store.SaveAuth(input)
 	if err != nil {
@@ -26,6 +22,15 @@ func (r *queryResolver) Auth(ctx context.Context, apiID string) (*model.Auth, er
 	return r.Store.Auth(apiID)
 }
 
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *authResolver) DeletePolicy(ctx context.Context, obj *model.Auth) (*model.AuthPolicy, error) {
+	panic(fmt.Errorf("not implemented"))
+}
 func (r *Resolver) Auth() generated.AuthResolver { return &authResolver{r} }
 
 type authResolver struct{ *Resolver }
