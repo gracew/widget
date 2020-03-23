beb0350d5beb52e3d0ae44f9e70acfe7db9a16ac
Grace Wang <gracewang92@gmail.com>
2020-03-22 17:02:51 -0700

    sigh

--- graph/auth.resolvers.go
+++ graph/auth.resolvers.go
@@ -4,9 +4,7 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/gracew/widget/graph/generated"
 	"github.com/gracew/widget/graph/model"
 )
 
@@ -21,16 +19,3 @@ func (r *mutationResolver) AuthAPI(ctx context.Context, input model.AuthAPIInput
 func (r *queryResolver) Auth(ctx context.Context, apiID string) (*model.Auth, error) {
 	return r.Store.Auth(apiID)
 }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *authResolver) DeletePolicy(ctx context.Context, obj *model.Auth) (*model.AuthPolicy, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *Resolver) Auth() generated.AuthResolver { return &authResolver{r} }
-
-type authResolver struct{ *Resolver }
