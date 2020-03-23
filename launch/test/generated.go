9c718bbb968117cf6158f83a6592c6900adbac31
Grace Wang <gracewang92@gmail.com>
2020-03-22 20:24:38 -0700

    set model table name to API name (#7)

--- launch/test/generated.go
+++ launch/test/generated.go
@@ -1,11 +1,13 @@
 package generated
 
 type Object struct {
-	ID        string  `json:"id" sql:"type:uuid,default:gen_random_uuid()"`
-	CreatedBy string  `json:"createdBy"`
-	Foo       bool    `json:"foo"`
-	Bar       float64 `json:"bar"`
-	Baz       int32   `json:"baz"`
-	Qux       string  `json:"qux"`
-	CamelCase string  `json:"camelCase"`
+	tableName struct{} `sql:"foo"`
+	ID        string   `json:"id" sql:"type:uuid,default:gen_random_uuid()"`
+	CreatedBy string   `json:"createdBy"`
+	CreatedAt string   `json:"createdAt" sql:"default:now()"`
+	Foo       bool     `json:"foo"`
+	Bar       float64  `json:"bar"`
+	Baz       int32    `json:"baz"`
+	Qux       string   `json:"qux"`
+	CamelCase string   `json:"camelCase"`
 }
